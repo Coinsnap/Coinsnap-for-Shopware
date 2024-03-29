10551fd281c8521e7a791651128c66ccc2140c65
Nemanja <nemanja-z@proton.me>
2024-03-29 13:13:48 +0100

    Remove payment methods

--- src/PaymentMethod/PaymentMethods.php
+++ src/PaymentMethod/PaymentMethods.php
@@ -12,25 +12,11 @@ declare(strict_types=1);
 
 namespace Coinsnap\Shopware\PaymentMethod;
 
-use Coinsnap\Shopware\PaymentMethod\BitcoinPaymentMethod;
-use Coinsnap\Shopware\PaymentMethod\LightningPaymentMethod;
-use Coinsnap\Shopware\PaymentMethod\BitcoinLightningPaymentMethod;
-use Coinsnap\Shopware\PaymentMethod\CoinsnapBitcoinPaymentMethod;
-use Coinsnap\Shopware\PaymentMethod\CoinsnapLightningPaymentMethod;
 use Coinsnap\Shopware\PaymentMethod\CoinsnapBitcoinLightningPaymentMethod;
-use Coinsnap\Shopware\PaymentMethod\MoneroPaymentMethod;
-use Coinsnap\Shopware\PaymentMethod\LitecoinPaymentMethod;
 
 class PaymentMethods
 {
     public const PAYMENT_METHODS = [
-        CoinsnapBitcoinPaymentMethod::class,
-        CoinsnapLightningPaymentMethod::class,
-        // CoinsnapBitcoinLightningPaymentMethod::class,
-        BitcoinPaymentMethod::class,
-        LightningPaymentMethod::class,
-        BitcoinLightningPaymentMethod::class,
-        MoneroPaymentMethod::class,
-        LitecoinPaymentMethod::class,
+        CoinsnapBitcoinLightningPaymentMethod::class,
     ];
 }
