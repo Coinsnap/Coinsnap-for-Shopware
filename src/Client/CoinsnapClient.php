4b603e1911e25cb3232af59d4c67fc52fa1cb5ff
Nemanja <nemanja-z@proton.me>
2024-04-12 14:59:12 +0200

    Update comments

--- src/Client/CoinsnapClient.php
+++ src/Client/CoinsnapClient.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * Copyright (c) 2023 Coinsnap
+ * Copyright (c) 2024 Coinsnap
  * This file is open source and available under the MIT license.
  * See the LICENSE file for more info.
  *
@@ -29,11 +29,11 @@ class CoinsnapClient extends AbstractClient implements ClientInterface
 
         $client = new Client(
             [
-            'base_uri' => 'https://app.coinsnap.io',
-            'headers' => [
-              'X-Api-Key' => $authorizationHeader
+                'base_uri' => 'https://app.coinsnap.io',
+                'headers' => [
+                    'X-Api-Key' => $authorizationHeader
+                ]
             ]
-      ]
         );
         parent::__construct($client, $logger);
     }
@@ -41,15 +41,15 @@ class CoinsnapClient extends AbstractClient implements ClientInterface
     {
         $headers['content-type'] = 'application/json';
         $options = [
-          'headers' => $headers,
-          'json'  => $data
+            'headers' => $headers,
+            'json'  => $data
         ];
         return $this->post($resourceUri, $options);
     }
     public function sendGetRequest(string $resourceUri, array $headers = []): array
     {
         $options = [
-          'headers' => $headers
+            'headers' => $headers
         ];
         return $this->get($resourceUri, $options);
     }
